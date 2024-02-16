22a3ddfd96abce85c5d05629ad13e936951e3ad9
Blake Moore <124715114+jblake46@users.noreply.github.com>
2024-02-16 16:42:39 -0500

    Delete CPSC2310/Lab4 directory

--- CPSC2310/Lab4/driver.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-//#include "functions.h"
-#include "src/functions.h"
-int main(int argc, char* argv[])
-{
-    printf("%d\n",sum(5, 10));
-    return 0;
-}
--- CPSC2310/Lab4/src/functions.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-int sum(int, int);
-
-#endif
